b63d0209b7752bc597fb98abe848d1c0e7b17501
ddobi <qhdqhdekd261@naver.com>
2023-01-31 20:03:41 +0900

    developing add menu

---
+++ src/components/meals/AddMenu.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Modal from "../UI/Modal";
+
+const AddMenu = (props) => {
+  return (
+    <Modal onClose={props.onClose}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </Modal>
+  );
+};
+
+export default AddMenu;
--- src/components/meals/AvailableMeals.js
+++ src/components/meals/AvailableMeals.js
@@ -1,35 +1,47 @@
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
+import { useState } from "react";
+import AddMenu from "./AddMenu";
 
 const DUMMY_MEALS = [
   {
     id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
+    name: "초밥",
+    description: "초밥 한 점에 밥알이 몇 개고?",
     price: 22.99,
   },
   {
     id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
+    name: "자장면",
+    description: "춘장과 면의 조화로운 하모니",
     price: 16.5,
   },
   {
     id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
+    name: "햄버거",
+    description: "맥도날드 vs 버거킹",
     price: 12.99,
   },
   {
     id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
+    name: "샐러드",
+    description: "이건 무슨 맛으로 먹는걸까",
     price: 18.99,
   },
 ];
 
 const AvailableMeals = () => {
+  const [addMenuIsShown, setAddMenuIsShown] = useState(false);
+
+  const showAddMenuHandler = () => {
+    setAddMenuIsShown(true);
+  };
+
+  const hideAddMenuHandler = () => {
+    setAddMenuIsShown(false);
+  };
+
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
       key={meal.id}
@@ -40,11 +52,17 @@ const AvailableMeals = () => {
     />
   ));
 
+  const addMealsClickHandler = () => {
+    showAddMenuHandler();
+  };
+
   return (
     <section className={classes.meals}>
+      {addMenuIsShown && <AddMenu onClose={hideAddMenuHandler} />}
       <Card>
         <ul>{mealsList}</ul>
       </Card>
+      <button onClick={addMealsClickHandler}>음식 추가</button>
     </section>
   );
 };
