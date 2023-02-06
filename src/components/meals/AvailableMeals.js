d3a1d3a54ec2f2052c5133405d4f1af9e8d87e28
ddobi <qhdqhdekd261@naver.com>
2023-02-07 01:10:54 +0900

    파이어 베이스 연결 및 dummy meals 호출

--- src/components/meals/AvailableMeals.js
+++ src/components/meals/AvailableMeals.js
@@ -1,14 +1,35 @@
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NewMenu from "./NewMenu";
-import DUMMY_MEALS from "./dummy-meals";
 import Button from "../UI/Button";
 
 const AvailableMeals = () => {
-  const [dummyMeals, setDummyMeals] = useState(DUMMY_MEALS);
+  const [dummyMeals, setDummyMeals] = useState([]);
   const [newMenuIsShown, setNewMenuIsShown] = useState(false);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://udemy-food-order-study-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setDummyMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []);
 
   const showNewMenuHandler = () => {
     setNewMenuIsShown(true);
